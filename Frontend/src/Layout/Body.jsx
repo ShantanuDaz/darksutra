e4879e61ff651b6e3384a6ce6fa160e6149414b8
ShantanuDaz <shaunofshan@gmail.com>
2023-10-15 14:47:00 +0530

    social media added

--- Frontend/src/Layout/Body.jsx
+++ Frontend/src/Layout/Body.jsx
@@ -1,11 +1,12 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Social from "./Social";
 import "./Layout.css";
 const Body = () => {
   return (
     <>
       <Header />
-
+      <Social />
       <Footer />
     </>
   );
---
+++ Frontend/src/Layout/Social.jsx
@@ -0,0 +1,23 @@
+const Social = () => {
+  return (
+    <div id="social">
+      <a href="" className="glass">
+        <img src="./images/instagram.svg" alt="instagram" />
+      </a>
+      <a href="" className="glass">
+        <img src="./images/facebook.svg" alt="facebook" />
+      </a>
+      <a href="" className="glass">
+        <img src="./images/x.png" alt="twitter" />
+      </a>
+      <a href="" className="glass">
+        <img src="./images/linkedin.svg" alt="linkedin" />
+      </a>
+      <a href="" className="glass">
+        <img src="./images/youtube.svg" alt="youtube" />
+      </a>
+    </div>
+  );
+};
+
+export default Social;
