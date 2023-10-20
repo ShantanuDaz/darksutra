21311ec5ce34b85c0bbd21a2df1b81a438b93e5a
VishwaUpadhyayDev <vishwaupadhyay24@gmail.com>
2023-10-20 12:33:17 +0530

    Refactored pocketbase connection code

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { pocketBase } from "../../api/pocketbase";
+import pocketBase from "../../api/pocketbase";
 
 export default function Home() {
   useEffect(() => {
@@ -8,21 +8,17 @@ export default function Home() {
 
   const getAllCategories = async () => {
     try {
-      const pb = await pocketBase().getPbConn();
+      const pb = await pocketBase.getPbConn();
       const records = await pb.collection("Category").getFullList({
         sort: "-created",
       });
       console.log(records);
     } catch (error) {
       if (error.message === "Token expired") {
-        await pocketBase().refreshToken();
+        await pocketBase.refreshToken();
 
         // Reattempt to fetch records after refreshing the token
-        const records = await pb
-          .collection("Category")
-          .getFullList({ sort: "-created" });
-        console.log(records);
-        // return records;
+        getAllCategories();
       } else {
         throw error;
       }
--- Frontend/src/api/pocketbase.js
+++ Frontend/src/api/pocketbase.js
@@ -3,9 +3,11 @@ import PocketBase from "pocketbase";
 //Initializing pocketbase
 const pb = new PocketBase(import.meta.env.VITE_PB_URL);
 
-export const pocketBase = () => {
+const pocketBase = {
   //Function to get pocketbase connection and setting authToken in authStore
-  async function getPbConn() {
+  getPbConn: async () => {
+    if (pb.health.check()) return pb;
+
     const authData = await pb
       .collection("users")
       .authWithPassword(
@@ -17,18 +19,15 @@ export const pocketBase = () => {
     pb.authStore.save(authData.token, null);
 
     return pb;
-  }
+  },
 
   //Function to refresh authToken
-  async function refreshToken() {
+  refreshToken: async () => {
     const newToken = await pb.collection("users").authRefresh();
 
     // Saving the token in the authStore
     pb.authStore.save(newToken, null);
-  }
-
-  return {
-    getPbConn,
-    refreshToken,
-  };
+  },
 };
+
+export default pocketBase;
